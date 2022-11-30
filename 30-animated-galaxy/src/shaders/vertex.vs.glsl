a403b853b90f30fd7ba9087de7fa79c4d228a2b5
polarzero <0xpolarzero@gmail.com>
2022-11-30 20:00:38 +0100

    Add lesson 30 - animated galaxy

---
+++ 30-animated-galaxy/src/shaders/vertex.vs.glsl
@@ -0,0 +1,39 @@
+uniform float uSize;
+uniform float uTime;
+
+attribute float aRandomness;
+attribute float aScale;
+
+varying vec3 vColor;
+
+void main() {
+  /**
+   * Position
+   */
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+  // After modelPosition, the vertex has been positioned according to the
+  // position, rotation and scale of the mesh Rotate
+  // Rotate
+  float angle = atan(modelPosition.x, modelPosition.z);
+  float distanceToCenter = length(modelPosition.xz);
+  float angleOffset = (1.0 / distanceToCenter) * uTime * 0.3;
+  angle += angleOffset;
+  modelPosition.x = cos(angle) * distanceToCenter;
+  modelPosition.z = sin(angle) * distanceToCenter;
+
+  // Randomness
+  modelPosition.xyz += aRandomness;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+  gl_Position = projectedPosition;
+
+  /**
+   * Size
+   */
+  gl_PointSize = uSize * aScale;
+  gl_PointSize *= (1.0 / -viewPosition.z);
+
+  vColor = color;
+}
