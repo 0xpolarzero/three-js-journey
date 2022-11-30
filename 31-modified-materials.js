c9dd77f07540292846d589765a5f8f49613e528c
polarzero <0xpolarzero@gmail.com>
2022-11-30 20:00:54 +0100

    Add notes for lesson 32 - modified materials

---
+++ 31-modified-materials.js
@@ -0,0 +1,5 @@
+// * Modifying materials with shaders
+// https://threejs-journey.com/lessons/modified-materials
+
+// * Hooking the matefial (accessing the original shaders before compilation)
+// https://threejs-journey.com/lessons/modified-materials#hooking-the-material
